68d9f13b0bcd55ddce1a28a74f8684f12c3050a6
Luiz Rioja <lulyrioja@gmail.com>
2022-11-01 19:40:53 -0300

    chore: change application to typescript

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/loading";
+import { SignIn } from "./src/screens/SignIn";
+
+export default function App() {
+  const [fontIsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
+
+      {fontIsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
