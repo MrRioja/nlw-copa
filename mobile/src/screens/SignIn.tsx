f6411e990ad6a33490a23cbc15aa518ecdcdeec7
Luiz Rioja <lulyrioja@gmail.com>
2022-11-04 23:44:04 -0300

    feat(SignIn): add auth hook

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -3,8 +3,11 @@ import { Fontisto } from "@expo/vector-icons";
 
 import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
+  const { user, signIn } = useAuth();
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -13,6 +16,7 @@ export function SignIn() {
         mt={12}
         type="SECONDARY"
         title="Entrar com o Google"
+        onPress={signIn}
         leftIcon={
           <Icon as={Fontisto} name="google" color="white" fontSize="md" />
         }
