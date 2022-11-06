721e849c1e4d6ee299b46b8dc903f74d5c5db55d
Luiz Rioja <lulyrioja@gmail.com>
2022-11-06 19:01:46 -0300

    feat(Pools): finish screen

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,45 @@
-import { Icon, VStack } from "native-base";
+import { useCallback, useState } from "react";
 import { Octicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { Icon, useToast, VStack, FlatList } from "native-base";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
+import { api } from "../services/api";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+import { PoolCard, PoolCardPros } from "../components/PoolCard";
 
 export function Pools() {
+  const toast = useToast();
   const { navigate } = useNavigation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [pools, setPools] = useState<PoolCardPros[]>([]);
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get("/pools");
+
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        placement: "top",
+        bgColor: "red.500",
+        title: "Não foi possível carregar os bolões.",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPools();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -28,6 +61,27 @@ export function Pools() {
           }
         />
       </VStack>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          px={5}
+          data={pools}
+          keyExtractor={(item) => item.id}
+          _contentContainerStyle={{
+            pb: 100,
+          }}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          renderItem={({ item }) => (
+            <PoolCard
+              data={item}
+              onPress={() => navigate("details", { id: item.id })}
+            />
+          )}
+        />
+      )}
     </VStack>
   );
 }
